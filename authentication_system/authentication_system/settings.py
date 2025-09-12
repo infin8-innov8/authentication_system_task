5fc18b3b5240c6bd0459e550570aa1a83fefbd92
= <=>
2025-09-12 22:43:10 +0530

    the mysql server has been added.

--- authentication_system/authentication_system/settings.py
+++ authentication_system/authentication_system/settings.py
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'authentication_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'accounts_db',
+        'USER' : 'accountant',
+        'PASSWORD' : 'activ8*o', 
+        'HOST' : '',
+        'PORT' : '',
     }
 }
 
