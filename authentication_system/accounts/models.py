58e64ab106af4baf178cbc78dd3e074092363865
= <=>
2025-09-12 14:35:14 +0530

    model is created

--- authentication_system/accounts/models.py
+++ authentication_system/accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class UserAccount(AbstractUser) : 
+    phone_number = models.CharField(max_length=12, blank=True, null=True)
+    
+    def __str__(self):
+        return self.email
+    
+    class Meta : 
+        db_table = "User Accounts"  
+        verbose_name = "User Account"
+        verbose_name_plural = "User Account"
+
+    def save(self, *args, **kwargs) : 
+        if self.first_name : 
+            self.first_name = self.first_name.upper()
+
+        if self.first_name : 
+            self.last_name = self.last_name.upper()
+
+        super().save(*args, **kwargs)
